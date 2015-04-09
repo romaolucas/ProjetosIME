9cc1f24952a70d6ec1e71bf541b4e6d330bdddf5
Lucas Romao Silva <luck.romao@gmail.com>
2015-04-09 07:50:20 -0300

    Novo gitignore

--- Tarefa15/get.java
+++
@@ -1,9 +0,0 @@
-public Value get(String key) {
-   Node x = root;
-   for (int d = 0; d < key.length(); d++) {
-      if (x == null) return null;
-      char c = key.charAt(d);
-      x = x.next[c];
-   }
-   return x.val;
-}
